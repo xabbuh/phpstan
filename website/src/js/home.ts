6afd4b4c68d75d6f4528c5b6491fc19a473a964b
Ondrej Mirtes <ondrej@mirtes.cz>
2024-12-08 22:01:20 +0100

    Revert "Celebrating homepage"
    This reverts commit 50af451eb42f4e7288068cd14f27ec769721213e.

--- website/src/js/home.ts
+++ website/src/js/home.ts
@@ -1,36 +1,9 @@
 import $ from 'jquery';
 import * as ko from 'knockout';
 import {MainMenuViewModel} from './MainMenuViewModel';
-import { Fireworks } from 'fireworks-js';
 
 $(() => {
 	ko.applyBindings({
 		mainMenu: new MainMenuViewModel(),
 	});
-
-	const container = document.querySelector('.fireworks');
-	if (container !== null) {
-		const fireworks = new Fireworks(container);
-		fireworks.start();
-
-		const fireworksStop = document.querySelector('.fireworks-stop');
-		if (fireworksStop !== null) {
-			let fireworking = true;
-			document.addEventListener('scroll', (event) => {
-				const rect = fireworksStop.getBoundingClientRect();
-				const bottom = rect.bottom;
-				if (bottom > 0) {
-					if (!fireworking) {
-						fireworks.start();
-						fireworking = true;
-					}
-				} else {
-					if (fireworking) {
-						fireworks.stop();
-						fireworking = false;
-					}
-				}
-			});
-		}
-	}
 });
